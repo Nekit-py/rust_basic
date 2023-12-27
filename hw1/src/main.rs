016b3909f9e300e2253cd70e3a6da3f4c2ef0947
sidorovich_ns <61461523+Nekit-py@users.noreply.github.com>
2023-12-27 09:56:52 +0700

    clippy fix

--- hw1/src/main.rs
+++ hw1/src/main.rs
@@ -37,12 +37,12 @@ fn main() {
     println!("{:?}", double_int32(32));
 
     println!("{:?}", double_int64(100));
-    println!("{:?}", double_int64(2000_000_000));
+    println!("{:?}", double_int64(2_000_000_000));
 
     println!("{:?}", double_float32(3.33));
     println!("{:?}", double_float32(214748364.12123445));
 
-    println!("{:?}", double_float64(3.45647128394));
+    println!("{:?}", double_float64(3.456_471_2));
     println!("{}", int_plus_float_to_float(-3, 5.83489));
     println!("{}", int_plus_float_to_int(111111, -9.22222));
     println!("{}", tuple_sum((123, 2147483647)));
