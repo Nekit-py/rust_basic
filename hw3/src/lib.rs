d6c69f70c571e2c559a88c5f766f04f4c7e82ba3
sidorovich_ns <61461523+Nekit-py@users.noreply.github.com>
2024-01-22 10:19:33 +0700

    испрвление замечаний при PR

--- hw3/src/lib.rs
+++ hw3/src/lib.rs
@@ -1,37 +1,47 @@
+/// Принимает мутабельную ссылку на кортеж и bool значение.
+/// Если false, возвращает мутабельную ссылку на первый элемент кортежа.
+/// Если true, возвращает мутабельную ссылку на второй элемент кортежа.
 pub fn first_or_second<T>(tup: &mut (T, T), condition: bool) -> &mut T {
-    // Принимает мутабельную ссылку на кортеж и bool значение.
-    // Если false, возвращает мутабельную ссылку на первый элемент кортежа.
-    // Если true, возвращает мутабельную ссылку на второй элемент кортежа.
     match condition {
         true => &mut tup.0,
-        _ => &mut tup.1,
+        false => &mut tup.1,
     }
 }
 
+/// Принимает мутабельную ссылку на слайс и число N. Возвращает мутабельную ссылку на N-ый элемент.
 pub fn elem_mut_ref_by_idx<T>(slice: &mut [T], n: usize) -> &mut T {
-    // Принимает мутабельную ссылку на слайс и число N. Возвращает мутабельную ссылку на N-ый элемент.
     &mut slice[n]
 }
 
+/// Принимает слайс и число N. Возвращает ссылку на N-ый элемент слайса с конца.
 pub fn elem_mut_ref_by_idx_end<T>(slice: &mut [T], n: usize) -> &mut T {
-    // Принимает слайс и число N. Возвращает ссылку на N-ый элемент слайса с конца.
-    &mut slice[slice.len() - n]
+    if n == 0 {
+        &mut slice[slice.len() - 1]
+    }
+    else {
+        &mut slice[slice.len() - n]
+    }
 }
 
+/// Принимает слайс и число N. Возвращает два слайса с элементами:
+/// с нулевого по N-1;
+/// с N-го по последний;
 pub fn two_slices<T>(slice: &[T], n: usize) -> (&[T], &[T]) {
-    // Принимает слайс и число N. Возвращает два слайса с элементами:
-    // с нулевого по N-1;
-    // с N-го по последний;
-    (&slice[0..n], &slice[n..])
+    (&slice[..n], &slice[n..])
 }
 
-pub fn slice_array<T>(slice: &[T]) -> [&[T]; 4] {
-    // Принимает слайс и возвращает массив слайсов,
-    // содержащий четыре равные (насколько возможно) части исходного слайса.
-    let mut result = [&slice[0..0]; 4];
+/// Принимает слайс и возвращает массив слайсов,
+/// содержащий четыре равные (насколько возможно) части исходного слайса.
+pub fn slice_array<const N: usize, T>(slice: &[T]) -> [&[T]; N] {
+    let mut result = [&slice[..]; N]; //[&slice[0..0]; 4]
+
+    if N == 0 {
+        return result;
+    }
+
     let slice_len = slice.len();
-    let chank_size = slice_len / 4;
-    let mut remainder = slice_len % 4;
+    let chank_size = slice_len / N;
+    let mut remainder = slice_len % N;
     let mut start = 0;
 
     for s in &mut result {
@@ -61,27 +71,27 @@ mod tests {
 
     #[test]
     fn test_elem_mut_ref_by_idx() {
-        let mut list = vec!["321", "123", "444"];
+        let mut list = ["321", "123", "444"];
         assert_eq!(*elem_mut_ref_by_idx(&mut list[..], 2), "444");
     }
 
     #[test]
     #[should_panic]
     fn test_panic_elem_mut_ref_by_idx() {
-        let mut list = vec![11, 22, 33];
+        let mut list = [11, 22, 33];
         assert_eq!(*elem_mut_ref_by_idx(&mut list[..], 99999999), 33);
     }
 
     #[test]
     fn test_elem_mut_ref_by_idx_end() {
-        let mut list = vec!['a', '3', '!'];
+        let mut list = ['a', '3', '!'];
         assert_eq!(*elem_mut_ref_by_idx_end(&mut list[..], 2), '3');
     }
 
     #[test]
     #[should_panic]
     fn test_panic_elem_mut_ref_by_idx_end() {
-        let mut list = vec![0.33333, 1243.321, 8.567312];
+        let mut list = [0.33333, 1243.321, 8.567312];
         assert_eq!(*elem_mut_ref_by_idx_end(&mut list[..], 99999999), 8.567312);
     }
 
@@ -96,13 +106,13 @@ mod tests {
     #[test]
     fn test_slice_array() {
         let array = [1, 2, 3, 4, 5, 6, 7, 8];
-        assert_eq!(slice_array(&array), [&[1, 2], &[3, 4], &[5, 6], &[7, 8]]);
+        assert_eq!(slice_array::<4, i32>(&array), [&[1, 2], &[3, 4], &[5, 6], &[7, 8]]);
     }
 
     #[test]
     #[should_panic]
     fn panic_test_slice_array() {
         let array = [1, 2, 3];
-        assert_eq!(slice_array(&array), [&[1, 2], &[3, 4], &[5, 6], &[7, 8]]);
+        assert_eq!(slice_array::<4, i32>(&array), [&[1, 2], &[3, 4], &[5, 6], &[7, 8]]);
     }
 }
